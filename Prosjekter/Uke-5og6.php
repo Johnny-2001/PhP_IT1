cd1fb853814bf61113a7d263069eaecdb803da09
Johnny <johnnyleo@live.no>
2019-01-30 14:26:09 +0100

    Update

--- Prosjekter/Uke-5og6.php
+++ Prosjekter/Uke-5og6.php
@@ -1,24 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gangetabellen</title>
-    <style>
-    table{
-       border-collapse: collapse;
-    }
-    td{
-        border: 1px solid black;
-        width: 50px;
-        height: 50px;
-        text-align: center;
-    }
-    .rad{
-        background-color: #ACACAC;
-    }
-    </style>
+    <title>PhP: Uke 5-6</title>
+    <link rel="stylesheet" href="./Stilark/uke5-6.css">
 </head>
 <body>
 <form method="get" id="form">
